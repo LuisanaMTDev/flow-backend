896465e96c98286d7677086f8b00d6302a4e4cf6
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2025-09-25 20:22:01 -0400

    fix(server): fix typo

--- cmd/server.go
+++ cmd/server.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/LuisanaMTDev/flow-backend/internal/controllers"
-	"github.com/LuisanaMTDev/flow-backend/internal/middleware"
+	"github.com/LuisanaMTDev/flow-backend/internal/middlewares"
 )
 
 func main() {
@@ -14,10 +14,10 @@ func main() {
 	handlers.HandleFunc("POST /register", controllers.Register)
 	handlers.HandleFunc("POST /login", controllers.Login)
 	handlers.HandleFunc("POST /logout", controllers.Logout)
-	handlers.HandleFunc("GET /protected", middleware.MakeHandler(controllers.CheckAuthHealth, middleware.Authorization))
+	handlers.HandleFunc("GET /protected", middlewares.MakeHandler(controllers.CheckAuthHealth, middlewares.Authorization))
 	
 
-	corsMiddleware := middleware.CORSMiddleware(middleware.DefaultCORSConfig())
+	corsMiddleware := middlewares.CORSMiddleware(middlewares.DefaultCORSConfig())
 	handler := corsMiddleware(handlers)
 
 	server := http.Server{Handler: handler, Addr: ":4322"}
